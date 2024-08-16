7c814e17fe8be57624699cfae22eaddcebc7ab49
Newman Sánchez <newmanls@pm.me>
2024-08-16 14:01:35 -0400

    actualizadas constantes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Seguridad Informática';
+export const SITE_DESCRIPTION = 'Blog orientado a la seguridad informática.';
+export const SOCIAL_LINKS = [
+    {
+        url: "https://github.com/newmanls/blog-seguridad-informatica",
+        description: "Código fuente del sitio",
+        icon: "github",
+    }
+];
