4287e459216d14a4e359e359948990e40a4efd50
Newman Sánchez <newmanls@pm.me>
2024-08-16 14:36:13 -0400

    modificado código para funcionar con "base path"

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://newmanls.github.io',
-    //base: '/blog-seguridad-informatica', // habilitar para github pages
+    base: '/blog-seguridad-informatica',
     integrations: [mdx(), sitemap()],
 });
