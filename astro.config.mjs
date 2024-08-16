9e59b4e08ac0865b58a2fb52f244445479d3775f
Newman Sánchez <newmanls@pm.me>
2024-08-16 16:41:20 -0400

    funcion para agregar base a urls en el renderizado de md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,32 @@
 import { defineConfig } from 'astro/config';
+import { visit } from 'unist-util-visit';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+function addBaseUrlToMarkdownLocalUrls() {
+    function visitor(node) {
+        if (node.url.startsWith('http') || node.url.startsWith('/images/')) {
+            return
+        }
+
+        if (node.url.startsWith('/')) {
+            node.url = '/blog-seguridad-informatica' + node.url
+        }
+    }
+
+    function transform(tree) {
+        visit(tree, 'image', visitor)
+    }
+
+    return transform
+}
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://newmanls.github.io',
     base: '/blog-seguridad-informatica',
     integrations: [mdx(), sitemap()],
+    markdown: {
+        remarkPlugins: [[addBaseUrlToMarkdownLocalUrls, {}]],
+    },
 });
