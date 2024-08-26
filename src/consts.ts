574f9d5ef6b6c1d89ff96bafe6306e425b5c30c8
Newman Sánchez <newmanls@pm.me>
2024-08-26 14:35:45 -0400

    fix: actualizar nombre del blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Seguridad Informática';
+export const SITE_TITLE = 'Pilares de La Seguridad Informática';
 export const SITE_DESCRIPTION = 'Blog orientado a la seguridad informática.';
 export const SOCIAL_LINKS = [
     {
